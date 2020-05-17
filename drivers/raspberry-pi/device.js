ed3c64141f5dc51004bf76c9778d34b555089129
Anu Lind <anu-maria@hotmail.com>
2020-05-17 15:57:33 +0200

    MQTT connection

--- drivers/raspberry-pi/device.js
+++ drivers/raspberry-pi/device.js
@@ -1,11 +1,49 @@
 'use strict';
 
 const Homey = require('homey');
+const mqtt = require('mqtt');
 
 class RaspberryPiDevice extends Homey.Device {
 
 	onInit() {
 		this.log('RaspberryPiDevice inited');
+
+		this.log('Connecting to MQTT...');
+		const { host, port } = this.getSettings();
+
+		const client = mqtt.connect(`mqtt://${host}:${port}`);
+		this.client = client;
+
+		// incoming data...
+		client.on('message', (topic, message) => {
+			this.log('Got new message');
+			this.log(topic);
+			this.log(message.toString());
+		});
+
+		client.on('connect', () => {
+			this.log('MQTT connection established');
+			client.subscribe('AM2320', err => {
+				if (err) {
+					this.log(err);
+					client.end();
+					this.setWarning('MQTT failure, device not receiving data');
+				} else {
+					this.log('Subscribed to AM2320');
+				}
+			});
+		});
+	}
+
+	onDeleted() {
+		this.log('Disconnecting RaspberryPiDevice...');
+		if (this.client) {
+			try {
+				this.client.end();
+			} catch(err) {
+				this.log(err);
+			}
+		}
 	}
 }
 
