26dded97a04ac4844a44b785483d70ddc95c677b
Anu Lind <anu-maria@hotmail.com>
2020-06-11 10:13:43 +0200

    Added cancel & refresh buttons + new sensors

--- drivers/raspberry-pi/device.js
+++ drivers/raspberry-pi/device.js
@@ -18,7 +18,7 @@ class RaspberryPiDevice extends Homey.Device {
 
 		this.client.on('connect', () => {
 			this.log('MQTT connection established');
-			const topics = ['pi/sensors/#'];
+			const topics = ['pi/sensors/#', 'pi/events'];
 			this.client.subscribe(topics, err => {
 				if (err) {
 					this.log(err);
@@ -34,7 +34,7 @@ class RaspberryPiDevice extends Homey.Device {
 		this.registerMultipleCapabilityListener(pumpCapabilities, valueObj => {
 			const capability = Object.keys(valueObj)[0];
 			const pin = capability.split('.')[1];
-			const requestId = `homey-${Date.now()}-${Math.floor(Math.random() * 100_000)}`;
+			const requestId = this.generateRequestId();
 			const message = {
 				action: "run",
 				payload: {
@@ -44,44 +44,93 @@ class RaspberryPiDevice extends Homey.Device {
 				},
 				requestId,
 			}
-			this.setCapabilityValue(capability, true);
 			this.client.publish("pi/requests", JSON.stringify(message));
-			setTimeout(() => this.setCapabilityValue(capability, false), 5000);
+			return Promise.resolve();
+		});
+
+		this.registerCapabilityListener('button.refresh_readings', (value, opts) => {
+			const requestId = this.generateRequestId();
+			const message = {
+				action: "read",
+				payload: {},
+				requestId,
+			}
+			this.client.publish("pi/requests", JSON.stringify(message));
+			return Promise.resolve();
+		});
+
+		this.registerCapabilityListener('button.cancel', (value, opts) => {
+			const requestId = this.generateRequestId();
+			const message = { requestId }
+			this.client.publish("pi/abort", JSON.stringify(message));
 			return Promise.resolve();
 		});
 	}
 
 	handleMessage(topic, message) {
 		this.log(`Got new message on topic ${topic}`);
-		try {
-			const msg = message.toString();
-			this.log(msg);
-			const { data } = JSON.parse(msg);
-
-			switch (topic) {
-				case 'pi/sensors/AM2320/humidity':
-					this.setCapabilityValue('measure_humidity.AM2320', data);
-					break;
-				case 'pi/sensors/AM2320/temperature':
-					this.setCapabilityValue('measure_temperature.AM2320', data);
-					break;
-				case 'pi/sensors/BMP280/pressure':
-					this.setCapabilityValue('measure_pressure.BMP280', data);
-					break;
-				case 'pi/sensors/BMP280/temperature':
-					this.setCapabilityValue('measure_temperature.BMP280', data);
-					break;
-				case 'pi/sensors/HCSR04/range':
-					this.setCapabilityValue('meter_water.HCSR04', data);
-					break;
-				case 'pi/sensors/TSL2561/light':
-					this.setCapabilityValue('measure_luminance.TSL2561', data);
-					break;
-			}
-		} catch (err) {
+		return Promise.resolve(JSON.parse(message.toString()))
+			.then(msg => {
+				this.log('message:', msg);
+				switch (topic) {
+					case 'pi/sensors/AM2320/humidity':
+						return this.setCapabilityValue('measure_humidity.AM2320', msg.data);
+					case 'pi/sensors/AM2320/temperature':
+						return this.setCapabilityValue('measure_temperature.AM2320', msg.data);
+					case 'pi/sensors/BMP280/pressure':
+						return this.setCapabilityValue('measure_pressure.BMP280', msg.data);
+					case 'pi/sensors/BMP280/temperature':
+						return this.setCapabilityValue('measure_temperature.BMP280', msg.data);
+					case 'pi/sensors/MCP3008/temperature':
+						return this.setCapabilityValue('measure_temperature.analog', msg.data);
+					case 'pi/sensors/HCSR04/range':
+						return this.setCapabilityValue('meter_water.HCSR04', msg.data);
+					case 'pi/sensors/TSL2561/light':
+						return this.setCapabilityValue('measure_luminance.TSL2561', msg.data);
+					case 'pi/sensors/MCP3008/soil_moisture_1':
+						return this.setCapabilityValue('measure_soil_moisture.soil_moisture_1', msg.data);
+					case 'pi/sensors/MCP3008/soil_moisture_2':
+						return this.setCapabilityValue('measure_soil_moisture.soil_moisture_2', msg.data);
+					case 'pi/sensors/MCP3008/soil_moisture_3':
+						return this.setCapabilityValue('measure_soil_moisture.soil_moisture_3', msg.data);
+					case 'pi/sensors/MCP3008/soil_moisture_4':
+						return this.setCapabilityValue('measure_soil_moisture.soil_moisture_4', msg.data);
+					case 'pi/sensors/MCP23017/pump1':
+						return this.setCapabilityValue('onoff.pump1', msg.data === 1);
+					case 'pi/sensors/MCP23017/pump2':
+						return this.setCapabilityValue('onoff.pump2', msg.data === 1);
+					case 'pi/sensors/MCP23017/pump3':
+						return this.setCapabilityValue('onoff.pump3', msg.data === 1);
+					case 'pi/sensors/MCP23017/pump4':
+						return this.setCapabilityValue('onoff.pump4', msg.data === 1);
+					case 'pi/events':
+						return this.handleEvent(msg);
+				}
 			// Something went wrong, for example message not JSON
-			this.log(err);
+			}).catch(err => this.log(err));
+	}
+
+	handleEvent({ status, message, action, payload }) {
+		// If an error event is published (regardless of origin), display error to user
+		switch (status) {
+			case 'queued':
+			case 'initiated':
+			case 'completed':
+				// Ignoring...
+				break;
+			case 'rejected':
+			case 'canceled':
+			case 'failed':
+				const { pin, device } = payload;
+				const name = `${device}${pin ? ":" + pin : ""}`;
+				this.setWarning(`${name}:${action} - ${message}`).catch(err => this.log(err));
+				setTimeout(() => this.unsetWarning(), 10000);
+				break;
 		}
+	}
+
+	generateRequestId() {
+		return `homey-${Date.now()}-${Math.floor(Math.random() * 100_000)}`;
 	}
 
 	onDeleted() {
